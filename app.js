97c57572cf37fc1679e59e439844d9b7c9aaf743
Jahongir <nusratovjahongir@gmail.com>
2025-01-31 15:50:04 +0500

    feat: complete actives, application and footer sections responsive design.

--- app.js
+++ app.js
@@ -5,12 +5,18 @@ const NAV_ITEM = document.querySelectorAll(".nav-item");
 HAMBURGER.addEventListener("click", () => {
   HAMBURGER.classList.toggle("hamburger-open");
   NAV_ITEMS.classList.toggle("nav-open");
+  if (HAMBURGER.classList.contains("hamburger-open")) {
+    disableScroll();
+  } else {
+    enableScroll();
+  }
 });
 
 NAV_ITEM.forEach((item) => {
   item.addEventListener("click", () => {
     HAMBURGER.classList.remove("hamburger-open");
     NAV_ITEMS.classList.remove("nav-open");
+    enableScroll();
   });
 });
 
